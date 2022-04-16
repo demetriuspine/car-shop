e502607edee89a2fbab96794ae27354cc7bae6c1
lucas <mr.pine97@gmail.com>
2022-04-16 16:03:06 -0300

    feat: adiciona o service crud do mongo

---
+++ src/services/MongoService.ts
@@ -0,0 +1,30 @@
+import { Model } from '../interfaces/ModelInterface';
+import { ServiceError } from '../interfaces/ServiceError';
+
+abstract class Service<T> {
+  constructor(
+    protected model: Model<T>,
+  ) {}
+
+  public async create(obj: T): Promise<T | null | ServiceError> {
+    return this.model.create(obj);
+  }
+
+  public async read(): Promise<T[]> {
+    return this.model.read();
+  }
+
+  public async readOne(id: string): Promise<T | null | ServiceError> {
+    return this.model.readOne(id);
+  }
+
+  public async update(id: string, obj: T): Promise<T | null | ServiceError> {
+    return this.model.update(id, obj);
+  }
+
+  public async delete(id: string): Promise<T | null | ServiceError> {
+    return this.model.delete(id);
+  }
+}
+
+export default Service;
