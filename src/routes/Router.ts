281857c0dbc2b32300c2dacbc8f531cffb585a6b
lucas <mr.pine97@gmail.com>
2022-04-21 14:17:03 -0300

    feat: cria o router de Car

---
+++ src/routes/Router.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import Controller from '../controllers';
+
+export default class CustomRouter<T> {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRoute(
+    controller: Controller<T>,
+    route = controller.route,
+  ) {
+    this.router.get(route, controller.read);
+    this.router.get(`${route}/:id`, controller.readOne);
+    this.router.post(route, controller.create);
+    this.router.put(`${route}/:id`, controller.update);
+    this.router.delete(`${route}/:id`, controller.delete);
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
 import App from './app';
+import CarController from './controllers/CarController';
+import { Car } from './interfaces/CarInterface';
+import CustomRouter from './routes/Router';
 
 const server = new App();
+
+const carController = new CarController();
+
+const carRouter = new CustomRouter<Car>();
+
+carRouter.addRoute(carController);
+
+server.addRouter(carRouter.router);
 
 export default server;
